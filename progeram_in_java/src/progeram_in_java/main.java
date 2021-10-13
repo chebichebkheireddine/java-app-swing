88ae5919d8146c13a69b161a73abd2d5ae507cd9
chebicheb kheireddin <chebicheb kheireddin@DESKTOP-URVVD2J>
2021-10-13 21:54:28 +0100

    this all fail

---
+++ progeram_in_java/src/progeram_in_java/main.java
@@ -0,0 +1,17 @@
+package progeram_in_java;
+
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame=new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		System.out.println("chebicheb");
+		frame.setVisible(true);
+
+	}
+
+}
